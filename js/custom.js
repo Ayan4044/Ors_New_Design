c7b9c72ad084c43c2b156b47ae3c6b4a7fb1e711
Ayan404 <ayan.bhatt20@gmail.com>
2021-03-10 09:02:17 +0530

    Counter, Loader and Image responsive issues fixed

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+
+  // End Prealoader
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 9000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
   var i = 0,
     a = 0,
     isBackspacing = false,
