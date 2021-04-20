49ff4d83f5444286ef60982b54b864c427b34eca
Ayan404 <ayan.bhatt20@gmail.com>
2021-04-20 13:13:27 +0530

    Modal Dialog Changes
    The bottom sheet modal will open after shimmer is done

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+
+  $(window).load(function () { // makes sure the whole site is loaded
+    // $('#status').fadeOut(); // will first fade out the loading animation
+    // $('#preloader').delay(50).fadeOut(100); // will fade out the white DIV that covers the website.
+    // $('body').delay(50).css({
+    //   'overflow': 'visible'
+    // });
+
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+          $(this).remove();
+          $('#modal1').modal();
+          $('#modal1').modal('open');
+      });
+  }
+
+  
+  })
+
+
   var arrLang = new Array();
         arrLang['en'] = new Array();
         arrLang['hi'] = new Array();
